d8f72ae36c2b6e0b8433670bf68ee1a265ceda10
CarlosPTN <carlosmioli1@gmail.com>
2024-06-26 11:39:00 +0200

    added app.js to the main branch and deleted to the app.js from the backend folder

---
+++ app.js
@@ -0,0 +1,21 @@
+const mysql2 = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "/config.env" });
+
+let connection = mysql2.createConnection({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USERNAME_DB,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  //   console.log(process.env);
+  console.log("Loaded environment variables:", process.env);
+  console.log(process.env.HOST);
+  if (err) return console.error(err.message);
+
+  console.log("Connected to the MySQL server.");
+});
