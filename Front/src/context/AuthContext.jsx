ef0a1b744ae27fd527c6c62951a5ce2a171fdfd2
CarlosPTN <carlosmioli1@gmail.com>
2024-07-04 20:39:26 +0200

    Trying to merge all the work

--- Front/src/context/AuthContext.jsx
+++ Front/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  console.log("💥💥💥", setIsAuthenticated);
+
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
       {children}
